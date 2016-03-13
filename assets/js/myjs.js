8569c6ad41c0c8203c888f3ae1f4e1baf719d4bb
Noel Kim <balaya.kim@gmail.com>
2016-03-13 19:58:39 -0400

    removing carousel code

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,32 +1,3 @@
-// code for the projects carousel 
-$('.center').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
-
-
-// code to unhide a section as it appears in the window
 $(document).ready(function() {
     
     /* Every time the window is scrolled ... */
